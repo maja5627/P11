b15c6969c392f550583f83239f2f20edcd7fe0b8
joja6309 <joe.jackson@zeetogroup.com>
2018-11-11 12:50:14 -0800

    Working convertNum

--- src/P11.java
+++ src/P11.java
@@ -5,13 +5,36 @@ public class P11 implements IP11 {
 	@Override
 	public void printPattern(int n) {
 		// TODO Auto-generated method stub
+	    
+	    //Precondition: n>0
+	    //Postcondition: print a pattern of n+1 lines ( 0 to n ):
+	    //line i (i = 0 to n) has i stars ("*"), followed by (n-i) stripes ("-")
+//	    if(n > 0) {
+//	        System.out.print("");
+//	        printPattern(n)
+	        //    if current is 1, print 1 and return. //base case
+//
+//	    }
+	  
 
 	}
+   
 
 	@Override
 	public int convertNum(int[] num) {
-		// TODO Auto-generated method stub
-		return 0;
+	    
+		return Integer.valueOf(convertNum(num, 0, num.length - 1, ""));
+	}
+	
+	private String convertNum(int[] num, int atIndex, int lastIndex, String result) {
+	    
+	    String tempString = result + String.valueOf(num[atIndex]);
+	    if(atIndex == lastIndex) {
+	        return tempString;
+	    }else {
+	        return convertNum(num,atIndex +=1 ,lastIndex,tempString);
+	    }
+	   
 	}
 
 	@Override
---
+++ src/main.java
@@ -0,0 +1,8 @@
+
+public class main {
+    public static void main(String[] args){
+       P11 p11 = new P11();
+       int[] num123 = {1,2,3};
+       System.out.println(p11.convertNum(num123));
+    }
+}
