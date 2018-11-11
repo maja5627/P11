e16f547d40e9d27ea8051be3249da8d89fe7da51
joja6309 <joe.jackson@zeetogroup.com>
2018-11-11 13:14:11 -0800

    Partial intersection

--- src/P11.java
+++ src/P11.java
@@ -25,10 +25,27 @@ public class P11 implements IP11 {
 	    
 		return Integer.valueOf(convertNum(num, 0, num.length - 1, ""));
 	}
+	// Helper
+	// Recursive function that will call itself until base case is reached
+	// convertNum
+	// atIndex = 0
+	// value = 0
+	// tempString = 0 
+	//convertNum(num,atIndex +=1 ,lastIndex,tempString)
+	// atIndex = 1
+	// value = 1 
+	//tempString = 01
+	// convertNum(num,atIndex +=1 ,lastIndex,tempString)
+	// convertNum(num,atIndex +=1 ,lastIndex,tempString)
+	//convertNum(num,atIndex +=1 ,lastIndex,tempString)
+	// lastIndex == atIndex 
+	// 
+	// [0,1,2,3,4] 
 	
 	private String convertNum(int[] num, int atIndex, int lastIndex, String result) {
 	    
 	    String tempString = result + String.valueOf(num[atIndex]);
+	    
 	    if(atIndex == lastIndex) {
 	        return tempString;
 	    }else {
@@ -36,11 +53,28 @@ public class P11 implements IP11 {
 	    }
 	   
 	}
+	
+	// Helper
+	// [1,2,3,4,5,7]
+	// [2,1,3,4,5,6]
+	
+	// 1,2,3,4,5 
 
 	@Override
 	public ArrayList<String> intersection(ArrayList<String> AL1, ArrayList<String> AL2) {
 		// TODO Auto-generated method stub
 		return null;
+	}
+	
+	public ArrayList<String> intersectionHelper(ArrayList<String> AL1, ArrayList<String> AL2,int currentIndex,ArrayList<String> intersection){
+	    String currentValue = AL1.get(currentIndex);
+	    if(AL1.size() == currentIndex) {
+	        
+	    }
+	    if(AL2.contains(currentValue)){
+	        intersection.add(currentValue);
+	    }
+	    return null;
 	}
 
 }
